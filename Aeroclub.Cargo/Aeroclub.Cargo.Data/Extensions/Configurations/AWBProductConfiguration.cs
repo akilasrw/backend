36433aaff13862061b284736310ab073c4795d95
rajitha_live <rajithatcc@gmail.com>
2022-06-07 21:14:25 +0530

    AWB Stack bug fix

--- Aeroclub.Cargo/Aeroclub.Cargo.Data/Extensions/Configurations/AWBProductConfiguration.cs
+++ Aeroclub.Cargo/Aeroclub.Cargo.Data/Extensions/Configurations/AWBProductConfiguration.cs
@@ -9,9 +9,9 @@ namespace Aeroclub.Cargo.Data.Extensions.Configurations
         public void Configure(EntityTypeBuilder<AWBProduct> builder)
         {
             builder.Property(p => p.ProductRefNumber).HasColumnType("varchar(40)");
-            builder.Property(p => p.ProductName).HasColumnType("nvarchar(100)").IsRequired();
-            builder.Property(p => p.ProductType).HasColumnType("tinyint").IsRequired();
-            builder.Property(p => p.Quantity).HasColumnType("tinyint").IsRequired();
+            builder.Property(p => p.ProductName).HasColumnType("nvarchar(100)");
+            builder.Property(p => p.ProductType).HasColumnType("tinyint");
+            builder.Property(p => p.Quantity).HasColumnType("tinyint");
             builder.HasQueryFilter(p => !EF.Property<bool>(p, "IsDeleted"));
         }
     }
