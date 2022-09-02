279ed74d44a0f791fffffacb627cdc202a42255a
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2022-09-02 10:14:42 +0530

    Add new Columns to flight sector

---
+++ Aeroclub.Cargo/Aeroclub.Cargo.Data/Migrations/20220901115842_AddTime_Columns_to_FlightSector_table.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aeroclub.Cargo.Data.Migrations
+{
+    public partial class AddTime_Columns_to_FlightSector_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "ArrivalDateTime",
+                table: "FlightSectors",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "DepartureDateTime",
+                table: "FlightSectors",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("6062fc9c-6298-43b2-99f5-d56077ab813f"),
+                column: "ConcurrencyStamp",
+                value: "80d7de29-ec8b-4b94-8195-bc61dbe0360d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("b1fabea9-7111-4e8d-b0a4-16e55ad6106f"),
+                column: "ConcurrencyStamp",
+                value: "76db425f-c64c-4f04-840b-4fb56aec6013");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArrivalDateTime",
+                table: "FlightSectors");
+
+            migrationBuilder.DropColumn(
+                name: "DepartureDateTime",
+                table: "FlightSectors");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("6062fc9c-6298-43b2-99f5-d56077ab813f"),
+                column: "ConcurrencyStamp",
+                value: "89f747a4-9821-4652-bc16-b122a34a4266");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("b1fabea9-7111-4e8d-b0a4-16e55ad6106f"),
+                column: "ConcurrencyStamp",
+                value: "c211b714-58e9-4c67-93c7-0209920a9769");
+        }
+    }
+}
