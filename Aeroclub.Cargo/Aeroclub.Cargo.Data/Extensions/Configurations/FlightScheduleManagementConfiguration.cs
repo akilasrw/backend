7ab3d1f9f5cd2d77b81c832d35efb8b8f573ae78
rajitha_live <rajithatcc@gmail.com>
2022-09-05 13:46:24 +0530

    SCS-40 add table

---
+++ Aeroclub.Cargo/Aeroclub.Cargo.Data/Extensions/Configurations/FlightScheduleManagementConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Aeroclub.Cargo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aeroclub.Cargo.Data.Extensions.Configurations
+{
+    public class FlightScheduleManagementConfiguration : IEntityTypeConfiguration<FlightScheduleManagement>
+    {
+        public void Configure(EntityTypeBuilder<FlightScheduleManagement> builder)
+        {
+            builder.Property(p => p.DaysOfWeek).HasColumnType("varchar(20)").IsRequired();
+        }
+    }
+}
