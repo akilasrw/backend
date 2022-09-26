b30888c977a3a5d8ef62d1fed2dfc1960ceb7e39
rajitha_live <rajithatcc@gmail.com>
2022-09-26 14:25:31 +0530

    SCS-176 Entityies add

---
+++ Aeroclub.Cargo/Aeroclub.Cargo.Data/Extensions/Configurations/AgentRateManagementConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Aeroclub.Cargo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aeroclub.Cargo.Data.Extensions.Configurations
+{
+    public class AgentRateManagementConfiguration : IEntityTypeConfiguration<AgentRateManagement>
+    {
+        public void Configure(EntityTypeBuilder<AgentRateManagement> builder)
+        {
+            builder.Property(p => p.OriginAirportCode).HasColumnType("varchar(3)").IsRequired();
+            builder.Property(p => p.DestinationAirportCode).HasColumnType("varchar(3)").IsRequired();
+            builder.Property(p => p.OriginAirportName).HasColumnType("nvarchar(80)").IsRequired();
+            builder.Property(p => p.DestinationAirportName).HasColumnType("nvarchar(80)").IsRequired();
+
+        }
+    }
+}
---
+++ Aeroclub.Cargo/Aeroclub.Cargo.Data/Extensions/Configurations/AgentRateManagementHistoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Aeroclub.Cargo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aeroclub.Cargo.Data.Extensions.Configurations
+{
+    public class AgentRateManagementHistoryConfiguration : IEntityTypeConfiguration<AgentRateManagementHistory>
+    {
+        public void Configure(EntityTypeBuilder<AgentRateManagementHistory> builder)
+        {
+            builder.Property(p => p.OriginAirportCode).HasColumnType("varchar(3)").IsRequired();
+            builder.Property(p => p.DestinationAirportCode).HasColumnType("varchar(3)").IsRequired();
+            builder.Property(p => p.OriginAirportName).HasColumnType("nvarchar(80)").IsRequired();
+            builder.Property(p => p.DestinationAirportName).HasColumnType("nvarchar(80)").IsRequired();
+            builder.Property(p => p.WeightType).HasColumnType("tinyint").IsRequired();
+            builder.Property(p => p.CreatedUser).HasColumnType("nvarchar(100)").IsRequired();
+        }
+    }
+}
