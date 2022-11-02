a025aeb26fa952b39387f511f737f66823a3d28a
rajitha_live <rajithatcc@gmail.com>
2022-11-02 17:37:43 +0530

    MasterScheduleRM changes

--- Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/RequestModels/MasterScheduleRMs/MasterScheduleRM.cs
+++ Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/RequestModels/MasterScheduleRMs/MasterScheduleRM.cs
@@ -6,15 +6,20 @@ namespace Aeroclub.Cargo.Application.Models.RequestModels.MasterScheduleRMs
 {
     public class MasterScheduleRM
     {
-        [Required(ErrorMessage = "Schedule start date required")]
+        [Required(ErrorMessage = "Aircraft ID required.")]
+        public Guid AircraftId { get; set; }
+
+        [Required(ErrorMessage = "Schedule start date required.")]
         public DateTime ScheduleStartDate { get; set; }
         public DateTime ScheduleEndDate { get; set; }
         public TimeSpan ScheduleStartTime { get; set; }
         public double NumberOfHours { get; set; }
         public string DaysOfWeek { get; set; } = null!;
-        [Required(ErrorMessage = "Schedule status required")]
+
+        [Required(ErrorMessage = "Schedule status required.")]
         public ScheduleStatus ScheduleStatus { get; set; }
-        [Required(ErrorMessage = "Schedule calendar type required")]
+
+        [Required(ErrorMessage = "Schedule calendar type required.")]
         public CalendarType CalendarType { get; set; }
     }
 }
