6cf536180c33d0904459f83f074d4834c711bfcb
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2022-12-08 21:28:01 +0530

    Fleet mgt idle times changes

--- Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/ViewModels/FlightScheduleVMs/AircraftIdleReportVM.cs
+++ Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/ViewModels/FlightScheduleVMs/AircraftIdleReportVM.cs
@@ -21,32 +21,6 @@ namespace Aeroclub.Cargo.Application.Models.ViewModels.FlightScheduleVMs
         public ScheduleStatus ScheduleStatus { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
-        public List<Tuple<DateTime,DateTime, double>> IdleDateRangeList { get; set; } // TODO: remove
         public IEnumerable<AircraftIdleDateRange> AircraftIdleDateRangeList { get; set; }
-    }
-
-    public class AircraftIdleDateRange
-    {
-        public AircraftIdleDateRange()
-        {
-
-        }
-
-        //public AircraftIdleDateRange(AircraftIdleDateRange aircraftIdleDateRange) : this()
-        //{
-        //    StartTime = aircraftIdleDateRange.StartTime;
-        //    EndTime = aircraftIdleDateRange.EndTime;
-        //    TotalHours = aircraftIdleDateRange.TotalHours;
-        //}
-
-        public AircraftIdleDateRange(DateTime startTime, DateTime endTime, double totalHours): this()
-        {
-            StartTime= startTime;
-            EndTime = endTime;
-            TotalHours= totalHours;
-        }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public double TotalHours { get; set; }
     }
 }
