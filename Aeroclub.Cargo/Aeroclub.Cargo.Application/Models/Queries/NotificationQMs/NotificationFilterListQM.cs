e4911ed736a91d6e993f7749685caa16e019c7f3
rajitha_live <rajithatcc@gmail.com>
2023-01-03 14:35:19 +0530

    SCS-227

--- Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/Queries/NotificationQMs/NotificationFilterListQM.cs
+++ Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/Queries/NotificationQMs/NotificationFilterListQM.cs
@@ -1,11 +1,12 @@
-﻿using Aeroclub.Cargo.Application.Models.Core;
+﻿using Aeroclub.Cargo.Application.Enums;
+using Aeroclub.Cargo.Application.Models.Core;
 
 namespace Aeroclub.Cargo.Application.Models.Queries.NotificationRMs
 {
     public class NotificationFilterListQM : BasePaginationQM
     {
         public Guid UserId { get; set; }
-        public bool IsUnread { get; set; }
+        public NotificationFilterType filterType { get; set; }
 
     }
 }
