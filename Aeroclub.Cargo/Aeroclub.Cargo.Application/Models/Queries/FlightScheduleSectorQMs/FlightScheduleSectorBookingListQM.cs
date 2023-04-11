7e9362e0eaca838eec7898744833ee0ca8b0ae33
yohanwdev <102813313+yohanwdev@users.noreply.github.com>
2023-04-11 21:45:08 +0530

    stand by cargo view changes

--- Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/Queries/FlightScheduleSectorQMs/FlightScheduleSectorBookingListQM.cs
+++ Aeroclub.Cargo/Aeroclub.Cargo.Application/Models/Queries/FlightScheduleSectorQMs/FlightScheduleSectorBookingListQM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aeroclub.Cargo.Common.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,8 @@ namespace Aeroclub.Cargo.Application.Models.Queries.FlightScheduleSectorQMs
 {
     public class FlightScheduleSectorBookingListQM
     {
-        public string FlightNumber { get; set; } = null!;
-        public DateTime FlightDate { get; set; }
+        public string? FlightNumber { get; set; } = null;
+        public DateTime FlightDate { get; set; } = DateTime.MinValue;
+        public StandByStatus StandByStatus { get; set; } = StandByStatus.None;
     }
 }
--- Aeroclub.Cargo/Aeroclub.Cargo.Application/Specifications/CargoBookingFlightScheduleSectorSpecification.cs
+++ Aeroclub.Cargo/Aeroclub.Cargo.Application/Specifications/CargoBookingFlightScheduleSectorSpecification.cs
@@ -10,7 +10,9 @@ namespace Aeroclub.Cargo.Application.Specifications
     {
         public CargoBookingFlightScheduleSectorSpecification(FlightScheduleSectorBookingListQM query)
              : base(x => ((string.IsNullOrEmpty(query.FlightNumber) ||(x.FlightScheduleSector != null && x.FlightScheduleSector.FlightNumber != null && x.FlightScheduleSector.FlightNumber.Contains(query.FlightNumber))) &&
-           (query.FlightDate == DateTime.MinValue || (x.FlightScheduleSector != null && x.FlightScheduleSector.ScheduledDepartureDateTime.Date == query.FlightDate.Date))) && (!x.FlightScheduleSector.IsDeleted))
+           (query.FlightDate == DateTime.MinValue || (x.FlightScheduleSector != null && x.FlightScheduleSector.ScheduledDepartureDateTime.Date == query.FlightDate.Date))) && 
+             (query.StandByStatus == Common.Enums.StandByStatus.None || x.CargoBooking.StandByStatus == query.StandByStatus)
+             && (!x.FlightScheduleSector.IsDeleted))
         {
             AddInclude(x => x.Include(y => y.CargoBooking).ThenInclude(x => x.PackageItems).ThenInclude(a => a.PackageULDContainers).ThenInclude(b => b.ULDContainer).ThenInclude(c => c.ULDContainerCargoPositions));
             AddInclude(x => x.Include(y => y.FlightScheduleSector));
