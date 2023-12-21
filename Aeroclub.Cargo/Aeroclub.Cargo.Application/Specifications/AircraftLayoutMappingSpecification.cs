a7909f219f5f37a3edd5684d171fdba86ccf2090
prabuddaFernando <prabuddafernando@gmail.com>
2023-12-21 16:47:52 +0100

    fix issue with flight creation issue.

--- Aeroclub.Cargo/Aeroclub.Cargo.Application/Specifications/AircraftLayoutMappingSpecification.cs
+++ Aeroclub.Cargo/Aeroclub.Cargo.Application/Specifications/AircraftLayoutMappingSpecification.cs
@@ -8,9 +8,15 @@ namespace Aeroclub.Cargo.Application.Specifications
     {
         public AircraftLayoutMappingSpecification(AircraftLayoutMappingQM query)
             : base(x => (query.AircraftTypeId == Guid.Empty || x.AircraftTypeId == query.AircraftTypeId) &&
-            (query.AircraftSubTypeId == Guid.Empty || x.AircraftSubTypeId == query.AircraftSubTypeId) || (query.FlightScheduleSectorId != Guid.Empty && x.AircraftSubType.FlightScheduleSectors.Any(y=> y.Id == query.FlightScheduleSectorId)))
+                (query.AircraftSubTypeId == Guid.Empty || x.AircraftSubTypeId == query.AircraftSubTypeId) || (query.FlightScheduleSectorId != Guid.Empty && x.AircraftSubType.FlightScheduleSectors.Any(y=> y.Id == query.FlightScheduleSectorId)))
         {
-
+    
+        }
+        
+        public AircraftLayoutMappingSpecification(AircraftLayoutSingleMappingQm query)
+            : base(x =>  (query.AircraftSubTypeId == Guid.Empty || x.AircraftSubTypeId == query.AircraftSubTypeId))
+        {
+        
         }
     }
 }
